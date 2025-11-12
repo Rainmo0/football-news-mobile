2cb9ceee28abbc3f1c42b886da1ab6f87482166d
Rainmo0 <aunknown145@gmail.com>
2025-11-12 14:33:52 +0700

    Tutorial 8 selesai

---
+++ lib/models/news_entry.dart
@@ -0,0 +1,55 @@
+// To parse this JSON data, do
+//
+//     final newsEntry = newsEntryFromJson(jsonString);
+
+import 'dart:convert';
+
+List<NewsEntry> newsEntryFromJson(String str) =>
+    List<NewsEntry>.from(json.decode(str).map((x) => NewsEntry.fromJson(x)));
+
+String newsEntryToJson(List<NewsEntry> data) =>
+    json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
+
+class NewsEntry {
+  String id;
+  String title;
+  String content;
+  String category;
+  String thumbnail;
+  int newsViews;
+  DateTime createdAt;
+  bool isFeatured;
+
+  NewsEntry({
+    required this.id,
+    required this.title,
+    required this.content,
+    required this.category,
+    required this.thumbnail,
+    required this.newsViews,
+    required this.createdAt,
+    required this.isFeatured,
+  });
+
+  factory NewsEntry.fromJson(Map<String, dynamic> json) => NewsEntry(
+    id: json["id"],
+    title: json["title"],
+    content: json["content"],
+    category: json["category"],
+    thumbnail: json["thumbnail"],
+    newsViews: json["news_views"],
+    createdAt: DateTime.parse(json["created_at"]),
+    isFeatured: json["is_featured"],
+  );
+
+  Map<String, dynamic> toJson() => {
+    "id": id,
+    "title": title,
+    "content": content,
+    "category": category,
+    "thumbnail": thumbnail,
+    "news_views": newsViews,
+    "created_at": createdAt.toIso8601String(),
+    "is_featured": isFeatured,
+  };
+}
